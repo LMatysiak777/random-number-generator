b2521e6ae8a74e67c33c2ca02f82f01570252cca
LMatysiak777 <lukmat7@gmail.com>
2021-12-03 11:13:30 +0100

    added script and simple stylesheet

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+const MIN_VALUE = 1;
+const MAX_VALUE = 100;
+const AMOUNT = 20;
+
+function generateNumbers() {
+  clearTable();
+  const oddNumbers = [];
+  const evenNumbers = [];
+  for (let i = 0; i < AMOUNT; i++) {
+    let generatedNumber = Math.floor(Math.random() * MAX_VALUE) + 1;
+    generatedNumber % 2 == 0
+      ? evenNumbers.push(generatedNumber)
+      : oddNumbers.push(generatedNumber);
+  }
+  evenNumbers.sort((x, y) => {return x - y;});
+  oddNumbers.sort((x, y) => {return x - y;});
+  return generateTable(evenNumbers, oddNumbers)
+}
+
+function generateTable(evenNumbers, oddNumbers) {
+  let rowsToGenerate = Math.max(evenNumbers.length, oddNumbers.length);
+  let table = document.querySelector("#table-results");
+ 
+  for (let i = 0; i < rowsToGenerate; i++) {
+    let newRow = document.createElement("tr");
+    let newRowCol1 = document.createElement("td");
+    if (evenNumbers[i]) newRowCol1.innerHTML = evenNumbers[i];
+    let newRowCol2 = document.createElement("td");
+    if (oddNumbers[i]) newRowCol2.innerHTML = oddNumbers[i];
+    newRow.appendChild(newRowCol1);
+    newRow.appendChild(newRowCol2);
+    table.appendChild(newRow);
+  }
+}
+
+function clearTable(){
+    let tableRows = document.querySelector("#table-results");
+    tableRows.innerHTML="";
+}
